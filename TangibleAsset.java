65cc295019c658034665e942e225067831966fc8
owataZ <oohama.tsurumi@gmail.com>
2025-04-24 11:17:15 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,9 @@
+public abstract class TangibleAsset {
+private String name;
+private int price; private String color;
+public TangibleAsset(String name, int price, String color) {
+this.name = name;
+this.price = price;
+this.color = color;
+}
+public String getName() { retum this.name; › public int getPrice) { return this.price; } public String getColor() { retum this.color; }
